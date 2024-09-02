5cdfc68f4480d5f6010423e5f1bdf5cceb05c259
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-09-02 11:21:01 -0700

    init

--- ReflectionModifier/ContentView.swift
+++ ReflectionModifier/ContentView.swift
@@ -9,13 +9,39 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 40) {
+            Text("SwiftUI")
+                .font(.system(size: 100))
+                .foregroundStyle(.blue.gradient)
+                .reflection(spacing: -20)
         }
         .padding()
+    }
+}
+
+struct ReflectionModifier: ViewModifier {
+    var opacity: Double
+    var spacing: CGFloat
+    
+    func body(content: Content) -> some View {
+        VStack {
+            content
+            content
+                .scaleEffect(-1)
+                .mask(
+                    LinearGradient(gradient: Gradient(colors: [.black.opacity(0.6), .black.opacity(0)]), startPoint: .top, endPoint: .bottom)
+                )
+                .opacity(opacity)
+                .offset(y: spacing)
+                .rotation3DEffect(.degrees(180), axis: (x: 0.0, y: 1.0, z: 0.2))
+                .scaleEffect(1.09)
+        }
+    }
+}
+
+extension View {
+    func reflection(opacity: Double = 0.4, spacing: CGFloat = 0) -> some View {
+        self.modifier(ReflectionModifier(opacity: opacity, spacing: spacing))
     }
 }
 
