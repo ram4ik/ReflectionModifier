d23960636ae1136cfeac8f2a1fa6c93c7ef02f27
Ramill Ibragimov <ribragimov@malwarebytes.com>
2024-09-02 11:01:46 -0700

    Initial Commit

---
+++ ReflectionModifier/ReflectionModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReflectionModifierApp.swift
+//  ReflectionModifier
+//
+//  Created by test on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct ReflectionModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
